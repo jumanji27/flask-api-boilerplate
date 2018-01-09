ab19855e3222b8a04b7fd4c0362b9a9cf318b0cf
Nikita Lebedev <n.lebedev@carprice.ru>
2018-01-09 18:36:59 +0300

    gunicorn

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import init
 import api.v1.routes.init
 import api.v1_1.routes.init
-from api.init import api
+from api.init import api as app
 
 
 if __name__ == '__main__':
-    api.run(
-        host=api.config['HOST'],
-        port=api.config['PORT'],
+    app.run(
+        host=app.config['HOST'],
+        port=app.config['PORT'],
     )
